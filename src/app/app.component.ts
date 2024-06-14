ea40f2a8bd9e24c23b1637e735977ca7441d41c9
MattFr <matthieu_franck@orange.fr>
2024-06-14 19:14:55 +0200

    added inputs and method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+interface AnnualData {
+  year: number;
+  interest: number;
+  valueEndOfYear: number;
+  annualInvestment: number;
+  totalInterest: number;
+  totalAmountInvested: number;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'investcalc';
+  initialInvestment = 0;
+  annualInvestment = 0;
+  expectedReturn = 0;
+  duration = 0;
+  annualData: AnnualData[] = [];
+
+  onSubmit() {
+    let investmentValue = this.initialInvestment;
+
+    for (let i = 0; i < this.duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear =
+        investmentValue * (this.expectedReturn / 100);
+      investmentValue += interestEarnedInYear + this.annualInvestment;
+      const totalInterest =
+        investmentValue - this.annualInvestment * year - this.initialInvestment;
+      this.annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: this.annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested:
+          this.initialInvestment + this.annualInvestment * year,
+      });
+    }
+    console.log(this.annualData);
+  }
 }
